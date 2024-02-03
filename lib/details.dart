d98ab39cab4da7e330dfec931377a999d37567e6
Great Grant <greatgrant116@gmail.com>
2024-02-03 17:12:06 +0100

    build details page

--- lib/details.dart
+++ lib/details.dart
@@ -1,13 +1,66 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
 
 import 'models/data.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final Animal animal;
-  const DetailsPage({super.key, required this.animal});
 
+  const DetailsPage({super.key, required this.animal});
+  @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          CachedNetworkImage(imageUrl: widget.animal.animalImage!,height: 300,width:double.infinity,fit: BoxFit.cover,),
+          Padding(
+            padding: const EdgeInsets.only(top: 275),
+            child: Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(30),topRight: Radius.circular(30),
+                  ),
+                  color: Colors.orange
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(widget.animal.animalName!,style: const TextStyle(fontSize: 30,color: Colors.white,fontWeight: FontWeight.bold),),
+                    const SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(widget.animal.animalInfo!,style: const TextStyle(fontSize: 18,color: Colors.white,)),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+          ),
+
+          Positioned(
+            top: 40,
+            left: 16,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: const Icon(
+                Icons.arrow_circle_left_outlined,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+
+
+        ],
+      ),
+    );
   }
 }
